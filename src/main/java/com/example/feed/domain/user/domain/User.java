f1cd4d40b02c9308c39bcbbc86123b13bcf1475c
keisa <kesksjxhx@gmail.com>
2022-10-08 13:01:50 +0900

    User

---
+++ src/main/java/com/example/feed/domain/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.example.feed.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String email;
+
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    @Column(length = 10, nullable = false)
+    private String name;
+
+    @Builder
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
