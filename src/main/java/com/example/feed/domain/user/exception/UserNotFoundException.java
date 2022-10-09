a5d4e0fb039021919b31b053f7322b8b15305389
keisa <kesksjxhx@gmail.com>
2022-10-09 09:52:26 +0900

    🗂 :: Exception 추가

---
+++ src/main/java/com/example/feed/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.feed.domain.user.exception;
+
+import com.example.feed.global.error.CustomException;
+import com.example.feed.global.error.ErrorCode;
+
+public class UserNotFoundException extends CustomException {
+
+    public static final CustomException EXCEPTION =
+            new UserNotFoundException();
+
+    private UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}
