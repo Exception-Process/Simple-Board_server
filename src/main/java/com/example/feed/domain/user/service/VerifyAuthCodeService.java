6b0d6ceab1570c3bf0c29c6b235c38f2966c5943
keisa <kesksjxhx@gmail.com>
2022-10-09 09:54:31 +0900

    🧩 :: VerifyAuthCodeService 작성

---
+++ src/main/java/com/example/feed/domain/user/service/VerifyAuthCodeService.java
@@ -0,0 +1,20 @@
+package com.example.feed.domain.user.service;
+
+import com.example.feed.domain.user.controller.dto.request.VerifyAuthCodeRequest;
+import com.example.feed.domain.user.domain.AuthCode;
+import com.example.feed.domain.user.facade.AuthCodeFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class VerifyAuthCodeService {
+
+    private final AuthCodeFacade authCodeFacade;
+
+    @Transactional
+    public void execute(VerifyAuthCodeRequest request) {
+        authCodeFacade.verifyAuthCode(request.getAuthCode(), request.getEmail());
+    }
+}
