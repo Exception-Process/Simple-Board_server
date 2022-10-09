d750cb781f30badac9b6626f82077d1281ca98bf
keisa <kesksjxhx@gmail.com>
2022-10-09 09:56:13 +0900

    🧩 :: SIgnUpService 구현

---
+++ src/main/java/com/example/feed/domain/user/service/SignUpService.java
@@ -0,0 +1,36 @@
+package com.example.feed.domain.user.service;
+
+import com.example.feed.domain.user.controller.dto.request.SignUpRequest;
+import com.example.feed.domain.user.domain.User;
+import com.example.feed.domain.user.domain.repository.UserRepository;
+import com.example.feed.domain.user.exception.UserAlreadyExistException;
+import com.example.feed.domain.user.facade.AuthCodeFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SignUpService {
+
+    private final AuthCodeFacade authCodeFacade;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void execute(SignUpRequest request) {
+
+        authCodeFacade.checkEmailDomain(request.getEmail());
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw UserAlreadyExistException.EXCEPTION;
+        }
+
+        authCodeFacade.checkIsVerified(request.getEmail());
+
+        userRepository.save(User.builder()
+                .email(request.getEmail())
+                .name(request.getName())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build());
+    }
+}
