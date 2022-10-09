99f681412906128593b010900db28f221c97bf5c
keisa <kesksjxhx@gmail.com>
2022-10-09 10:34:34 +0900

    🧩 :: AuthController 작성

---
+++ src/main/java/com/example/feed/domain/auth/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.feed.domain.auth.controller;
+
+import com.example.feed.domain.auth.controller.dto.request.LoginRequest;
+import com.example.feed.domain.auth.controller.dto.response.LoginResponse;
+import com.example.feed.domain.auth.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@RestController
+public class AuthController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public LoginResponse login(LoginRequest request) {
+        return loginService.execute(request);
+    }
+}
