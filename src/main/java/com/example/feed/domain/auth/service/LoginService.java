00626a42cbe943371766c081dfcae06a94001e26
keisa <kesksjxhx@gmail.com>
2022-10-09 10:34:22 +0900

    🧩 :: LoginService 구현

---
+++ src/main/java/com/example/feed/domain/auth/service/LoginService.java
@@ -0,0 +1,38 @@
+package com.example.feed.domain.auth.service;
+
+import com.example.feed.domain.auth.controller.dto.request.LoginRequest;
+import com.example.feed.domain.auth.controller.dto.response.LoginResponse;
+import com.example.feed.domain.auth.exception.InvalidPasswordException;
+import com.example.feed.domain.user.domain.User;
+import com.example.feed.domain.user.facade.UserFacade;
+import com.example.feed.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserFacade userFacade;
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginResponse execute(LoginRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        User user = userFacade.getUserByEmail(email);
+
+        if (!passwordEncoder.matches(password, user.getPassword()))
+            throw InvalidPasswordException.EXCEPTION;
+
+        String accessToken = jwtTokenProvider.generateAccessToken(email);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(email);
+
+        return LoginResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}
