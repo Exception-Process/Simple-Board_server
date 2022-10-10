022c2994ff501d41b0c717dccfa341c1f719baf9
keisa <kesksjxhx@gmail.com>
2022-10-10 22:42:39 +0900

    feat :: BoardDetailService 작성

---
+++ src/main/java/com/example/feed/domain/board/service/QueryBoardDetailService.java
@@ -0,0 +1,29 @@
+package com.example.feed.domain.board.service;
+
+import com.example.feed.domain.board.controller.dto.response.BoardDetailResponse;
+import com.example.feed.domain.board.domain.Board;
+import com.example.feed.domain.board.facade.BoardFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class QueryBoardDetailService {
+
+    private BoardFacade boardFacade;
+
+    @Transactional(readOnly = true)
+    public BoardDetailResponse execute(Long id) {
+
+        Board board = boardFacade.getBoardById(id);
+
+        return BoardDetailResponse.builder()
+                .boardId(board.getId())
+                .title(board.getTitle())
+                .introduction(board.getIntroduction())
+                .adminName(board.getAdmin().getName())
+                .createdAt(board.getCreatedAt())
+                .build();
+    }
+}
