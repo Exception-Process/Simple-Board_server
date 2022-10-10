8578525c83ccc1aa981d47689b2e7523ce63fb3b
keisa <kesksjxhx@gmail.com>
2022-10-10 22:41:27 +0900

    feat :: BoardRepository 추가

--- src/main/java/com/example/feed/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/example/feed/domain/board/domain/repository/BoardRepository.java
@@ -3,5 +3,9 @@ package com.example.feed.domain.board.domain.repository;
 import com.example.feed.domain.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findAllByOrderByCreatedAtDesc();
 }
