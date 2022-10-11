b401b1a7cfd77f85ecf9895925f14570eedb1e71
keisa <kesksjxhx@gmail.com>
2022-10-11 18:45:43 +0900

    🧩 :: UpdateBoardRequest 추가

---
+++ src/main/java/com/example/feed/domain/board/controller/dto/request/UpdateBoardRequest.java
@@ -0,0 +1,11 @@
+package com.example.feed.domain.board.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateBoardRequest {
+
+    private String introduction;
+}
