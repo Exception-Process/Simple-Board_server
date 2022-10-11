81624742d4b4cec8e34e281db8035860efbefd81
keisa <kesksjxhx@gmail.com>
2022-10-11 18:48:18 +0900

    🧩 :: Controller 작성

--- src/main/java/com/example/feed/domain/board/controller/BoardController.java
+++ src/main/java/com/example/feed/domain/board/controller/BoardController.java
@@ -1,11 +1,10 @@
 package com.example.feed.domain.board.controller;
 
 import com.example.feed.domain.board.controller.dto.request.CreateBoardRequest;
+import com.example.feed.domain.board.controller.dto.request.UpdateBoardRequest;
 import com.example.feed.domain.board.controller.dto.response.BoardDetailResponse;
 import com.example.feed.domain.board.controller.dto.response.BoardListResponse;
-import com.example.feed.domain.board.service.CreateBoardService;
-import com.example.feed.domain.board.service.QueryBoardDetailService;
-import com.example.feed.domain.board.service.QueryBoardListService;
+import com.example.feed.domain.board.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +19,8 @@ public class BoardController {
     private final CreateBoardService createBoardService;
     private final QueryBoardDetailService queryBoardDetailService;
     private final QueryBoardListService queryBoardListService;
+    private final UpdateBoardService updateBoardService;
+    private final DeleteBoardService deleteBoardService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -35,5 +36,17 @@ public class BoardController {
     @GetMapping
     public BoardListResponse getList() {
         return queryBoardListService.execute();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{board-id}")
+    public void update(@RequestBody @Valid UpdateBoardRequest request, @PathVariable("board-id") Long id) {
+        updateBoardService.execute(request, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{board-id}")
+    public void delete(@PathVariable("board-id") Long id) {
+        deleteBoardService.execute(id);
     }
 }
