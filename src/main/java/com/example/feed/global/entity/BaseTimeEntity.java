7c81308675b23a0a6c38b83a0dc60bc3b3fe7814
keisa <kesksjxhx@gmail.com>
2022-10-11 18:43:11 +0900

    ⚙️ :: BaseTimeEntity 추상클래스로 수정

--- src/main/java/com/example/feed/global/entity/BaseTimeEntity.java
+++ src/main/java/com/example/feed/global/entity/BaseTimeEntity.java
@@ -5,17 +5,15 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @NotNull
     @CreatedDate
     private LocalDateTime createdAt;
-
 }
