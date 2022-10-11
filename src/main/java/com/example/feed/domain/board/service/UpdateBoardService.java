28ddb9a8021d978c35f25359b8fdced0674da548
keisa <kesksjxhx@gmail.com>
2022-10-11 18:47:09 +0900

    🧩 :: UpdateBoardService 구현

---
+++ src/main/java/com/example/feed/domain/board/service/UpdateBoardService.java
@@ -0,0 +1,30 @@
+package com.example.feed.domain.board.service;
+
+import com.example.feed.domain.board.controller.dto.request.UpdateBoardRequest;
+import com.example.feed.domain.board.domain.Board;
+import com.example.feed.domain.board.domain.repository.BoardRepository;
+import com.example.feed.domain.board.facade.BoardFacade;
+import com.example.feed.domain.user.domain.User;
+import com.example.feed.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateBoardService {
+
+    private final BoardFacade boardFacade;
+    private final UserFacade userFacade;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public void execute(UpdateBoardRequest request, Long id) {
+
+        User admin = userFacade.getUser();
+        Board board = boardFacade.getBoardByIdAndCheckAdmin(admin, id);
+
+        board.update(request.getIntroduction());
+        boardRepository.save(board);
+    }
+}
