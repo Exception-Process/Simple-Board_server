4b4bb4c2eeb194e2fecaf185909fa8387a5eb08a
keisa <kesksjxhx@gmail.com>
2022-10-12 17:13:14 +0900

    🧩 :: board 만들때 member에 admin 추가

--- src/main/java/com/example/feed/domain/board/service/CreateBoardService.java
+++ src/main/java/com/example/feed/domain/board/service/CreateBoardService.java
@@ -2,7 +2,10 @@ package com.example.feed.domain.board.service;
 
 import com.example.feed.domain.board.controller.dto.request.CreateBoardRequest;
 import com.example.feed.domain.board.domain.Board;
+import com.example.feed.domain.member.domain.Member;
 import com.example.feed.domain.board.domain.repository.BoardRepository;
+import com.example.feed.domain.member.domain.repository.MemberRepository;
+import com.example.feed.domain.member.domain.types.Authority;
 import com.example.feed.domain.user.domain.User;
 import com.example.feed.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +17,25 @@ public class CreateBoardService {
 
     private final UserFacade userFacade;
     private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
     public void execute(CreateBoardRequest request) {
 
         User user = userFacade.getUser();
 
-        boardRepository.save(Board.builder()
+        Board board = boardRepository.save(Board.builder()
                 .title(request.getTitle())
                 .introduction(request.getIntroduction())
                 .admin(user)
+                .build()
+        );
+
+        memberRepository.save(Member.builder()
+                .user(user)
+                .board(board)
+                .authority(Authority.ADMIN)
+                .join(true)
+                .name(request.getAdminName())
                 .build()
         );
     }
