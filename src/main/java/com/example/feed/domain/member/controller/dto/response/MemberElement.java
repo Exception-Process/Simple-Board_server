1904f7a307b007c4e1a1f781adac80b8937943b1
keisa <kesksjxhx@gmail.com>
2022-10-12 17:20:33 +0900

    🧩 :: MemberElement

---
+++ src/main/java/com/example/feed/domain/member/controller/dto/response/MemberElement.java
@@ -0,0 +1,14 @@
+package com.example.feed.domain.member.controller.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class MemberElement {
+
+    private Long id;
+    private String name;
+}
