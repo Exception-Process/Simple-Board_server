45e20a4f49b42398204fce778c81c22e7d86a8ee
keisa <kesksjxhx@gmail.com>
2022-10-16 21:31:54 +0900

    🗂 :: response

--- src/main/java/com/example/feed/domain/member/controller/dto/response/MemberDetailResponse.java
+++ src/main/java/com/example/feed/domain/member/controller/dto/response/MemberDetailResponse.java
@@ -1,2 +1,13 @@
-package com.example.feed.domain.member.controller.dto.response;public class MemberDetailResponse {
+package com.example.feed.domain.member.controller.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberDetailResponse {
+
+    private String userName;
+    private String authority;
+    private String memberName;
 }
