05151a29741e2a6511304a79e74dc078dcef3301
keisa <kesksjxhx@gmail.com>
2022-10-16 21:38:59 +0900

    🧩 :: 게시판 유저 리스트 조회

--- src/main/java/com/example/feed/domain/member/service/QueryBoardMemberListService.java
+++ src/main/java/com/example/feed/domain/member/service/QueryBoardMemberListService.java
@@ -1,2 +1,40 @@
-package com.example.feed.domain.member.service;public class QueryBoardMemberListService {
+package com.example.feed.domain.member.service;
+
+import com.example.feed.domain.board.domain.Board;
+import com.example.feed.domain.board.facade.BoardFacade;
+import com.example.feed.domain.member.controller.dto.response.MemberElement;
+import com.example.feed.domain.member.controller.dto.response.MemberListResponse;
+import com.example.feed.domain.member.domain.Member;
+import com.example.feed.domain.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class QueryBoardMemberListService {
+
+    private final MemberRepository memberRepository;
+    private final BoardFacade boardFacade;
+
+    public MemberListResponse execute(Long boardId) {
+
+        Board board = boardFacade.getBoardById(boardId);
+
+        List<MemberElement> memberList = memberRepository.findAllByBoard(board)
+                .stream()
+                .map(this::memberListBuilder)
+                .collect(Collectors.toList());
+
+        return new MemberListResponse(memberList);
+    }
+
+    private MemberElement memberListBuilder(Member member) {
+        return MemberElement.builder()
+                .id(member.getId())
+                .name(member.getName())
+                .build();
+    }
 }
