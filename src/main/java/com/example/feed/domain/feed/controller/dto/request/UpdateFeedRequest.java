794c3db3357b0eed400ce915756cd13dd4c8a4f6
keisa <kesksjxhx@gmail.com>
2022-10-20 21:54:54 +0900

    🧩 :: 게시글 수정 service 구현

---
+++ src/main/java/com/example/feed/domain/feed/controller/dto/request/UpdateFeedRequest.java
@@ -0,0 +1,24 @@
+package com.example.feed.domain.feed.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class UpdateFeedRequest {
+
+    @NotBlank
+    @Size(min = 1, max = 20, message = "공백, 널 포함 불가, 1~20자 사이")
+    private String title;
+
+    @NotBlank
+    @Size(min = 1, max = 20, message = "공백, 널 포함 불가, 1~1000자 사이")
+    private String content;
+
+    @NotNull(message = "널 포함 불가")
+    private boolean nonMemberShowing;
+}
---
+++ src/main/java/com/example/feed/domain/feed/service/UpdateFeedService.java
@@ -0,0 +1,32 @@
+package com.example.feed.domain.feed.service;
+
+import com.example.feed.domain.board.domain.Board;
+import com.example.feed.domain.board.facade.BoardFacade;
+import com.example.feed.domain.feed.controller.dto.request.UpdateFeedRequest;
+import com.example.feed.domain.feed.domain.Feed;
+import com.example.feed.domain.feed.domain.repository.FeedRepository;
+import com.example.feed.domain.feed.facade.FeedFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateFeedService {
+
+    private final FeedFacade feedFacade;
+    private final FeedRepository feedRepository;
+
+    @Transactional
+    public void execute(UpdateFeedRequest request, Long feedId) {
+
+        String title = request.getTitle();
+        String content = request.getContent();
+        boolean nonMemberShowing = request.isNonMemberShowing();
+
+        Feed feed = feedFacade.getFeed(feedId);
+
+        feed.update(title, content, nonMemberShowing);
+        feedRepository.save(feed);
+    }
+}
