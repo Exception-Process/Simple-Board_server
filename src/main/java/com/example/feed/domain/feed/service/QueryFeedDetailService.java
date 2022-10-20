baa277409f92d171e17da43f6bc18e68677074ec
keisa <kesksjxhx@gmail.com>
2022-10-20 21:55:47 +0900

    🧩 :: 게시글 상세조회 service 구현

---
+++ src/main/java/com/example/feed/domain/feed/service/QueryFeedDetailService.java
@@ -0,0 +1,32 @@
+package com.example.feed.domain.feed.service;
+
+import com.example.feed.domain.feed.controller.dto.response.FeedDetailResponse;
+import com.example.feed.domain.feed.domain.Feed;
+import com.example.feed.domain.feed.exception.CannotSeeFeedException;
+import com.example.feed.domain.feed.facade.FeedFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class QueryFeedDetailService {
+
+    private final FeedFacade feedFacade;
+
+    @Transactional(readOnly = true)
+    public FeedDetailResponse execute(Long feedId) {
+
+        Feed feed = feedFacade.getFeed(feedId);
+
+        if (!feed.isNonMemberShowing())
+            throw CannotSeeFeedException.EXCEPTION;
+
+        return FeedDetailResponse.builder()
+                .title(feed.getTitle())
+                .content(feed.getContent())
+                .memberName(feed.getMember().getName())
+                .createdAt(feed.getCreatedAt())
+                .build();
+    }
+}
