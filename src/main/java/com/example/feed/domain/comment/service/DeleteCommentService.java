995012feff2dc0dc92e6f8214e2cf05e711ebb91
keisa <kesksjxhx@gmail.com>
2022-10-24 08:16:19 +0900

    🧩 :: 댓글 삭제 기능 구현

---
+++ src/main/java/com/example/feed/domain/comment/service/DeleteCommentService.java
@@ -0,0 +1,20 @@
+package com.example.feed.domain.comment.service;
+
+import com.example.feed.domain.comment.domain.Comment;
+import com.example.feed.domain.comment.domain.repository.CommentRepository;
+import com.example.feed.domain.comment.facade.CommentFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteCommentService {
+
+    private final CommentFacade commentFacade;
+    private final CommentRepository commentRepository;
+
+    public void execute(Long commentId) {
+        Comment comment = commentFacade.getComment(id);
+        commentRepository.delete(comment);
+    }
+}
