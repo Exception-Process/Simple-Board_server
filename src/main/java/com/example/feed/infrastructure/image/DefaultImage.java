72ecce5a39b96cd12f1208ddcd7bdb0874e6d969
keisa <kesksjxhx@gmail.com>
2022-10-25 22:24:23 +0900

    🗂 :: defaultImage 추가

---
+++ src/main/java/com/example/feed/infrastructure/image/DefaultImage.java
@@ -0,0 +1,10 @@
+package com.example.feed.infrastructure.image;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DefaultImage {
+
+    public static final String USER_PROFILE_IMAGE = "'a'";
+}
