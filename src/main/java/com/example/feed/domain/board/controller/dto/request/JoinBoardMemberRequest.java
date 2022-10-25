79b812fae7fcd1b8e2a9481e4d478234a8ee5db4
keisa <kesksjxhx@gmail.com>
2022-10-25 10:53:28 +0900

    🛩 :: validation 추가

--- src/main/java/com/example/feed/domain/board/controller/dto/request/JoinBoardMemberRequest.java
+++ src/main/java/com/example/feed/domain/board/controller/dto/request/JoinBoardMemberRequest.java
@@ -3,12 +3,14 @@ package com.example.feed.domain.board.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class JoinBoardMemberRequest {
 
-    @Size(min = 1, max = 20)
+    @NotBlank
+    @Size(min = 1, max = 20, message = "1~20자, NULL이나 공백 포함 불가")
     private String name;
 }
