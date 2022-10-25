3e8d9f63a6c48043c0107da54576c087a084a0a2
keisa <kesksjxhx@gmail.com>
2022-10-25 11:14:56 +0900

    🛩 :: validation 추가

--- src/main/java/com/example/feed/domain/member/controller/dto/request/UpdateMemberRequest.java
+++ src/main/java/com/example/feed/domain/member/controller/dto/request/UpdateMemberRequest.java
@@ -3,9 +3,14 @@ package com.example.feed.domain.member.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor
 public class UpdateMemberRequest {
 
+    @NotBlank
+    @Size(min = 1, max = 20, message = "공백이나 널을 포함할 수 없고, 1~20자여야 됩니다.")
     private String name;
 }
