d42d79151389e3e009a25d5f35dbbf678f808f09
keisa <kesksjxhx@gmail.com>
2022-10-25 10:56:11 +0900

    🛩 :: validation 추가

--- src/main/java/com/example/feed/domain/board/controller/dto/request/UpdateBoardRequest.java
+++ src/main/java/com/example/feed/domain/board/controller/dto/request/UpdateBoardRequest.java
@@ -3,9 +3,14 @@ package com.example.feed.domain.board.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor
 public class UpdateBoardRequest {
 
+    @NotBlank
+    @Size(min = 1, max = 100, message = "소개글은 1~100자 사이여야되고, 공백이나 NULL을 포함할 수 없습니다.")
     private String introduction;
 }
