ed5c782bc7febc2627f625da18fbc699e0fdcc2d
keisa <kesksjxhx@gmail.com>
2022-10-28 13:48:38 +0900

    🗂 :: firebase 연동

---
+++ src/main/java/com/example/feed/infrastructure/fcm/FCMInitializer.java
@@ -0,0 +1,34 @@
+package com.example.feed.infrastructure.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.Arrays;
+
+@Component
+public class FCMInitializer {
+
+    @Value("${fcm.path}")
+    private String path;
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(path).getInputStream()))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
